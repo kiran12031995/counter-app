537329737ad7a089ad84d7dc3ca7a6c7e9013464
Kiran.Kumari <kiran12031995@gmail.com>
2025-07-11 16:07:06 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counter-app';
+  history:any = []
+
+  count = 0;
+
+  increment() {
+    this.count++;
+    this.history.push(`[+1] counter is now ${this.count}`)
+  }
+
+  decrement() {
+    if(this.count !== 0 || this.count > 0) {
+      this.count--;
+      this.history.push(`[-1] counter is now ${this.count}`)
+    }
+  }
+
+  reset(){
+    if(this.count !==0) {
+      this.count = 0;
+      this.history.push(`[Reset] counter is now ${this.count}`)
+    }
+  }
 }
